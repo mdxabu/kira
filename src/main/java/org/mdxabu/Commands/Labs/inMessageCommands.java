3fee5a035561c882bfe737ddbe61ef4fe6e96019
Abu <abdullahfakrudeen2020@gmail.com>
2026-02-06 20:48:27 +0530

    Added a new ?roll command
    Signed-off-by: Abu <abdullahfakrudeen2020@gmail.com>

--- src/main/java/org/mdxabu/Commands/Labs/inMessageCommands.java
+++ src/main/java/org/mdxabu/Commands/Labs/inMessageCommands.java
@@ -7,11 +7,12 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Random;
 
 public class inMessageCommands extends ListenerAdapter {
+    Random random = new Random();
+
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         if(event.getMessage().getContentRaw().equals("?flip")){
-            Random random = new Random();
             String coin;
 
             int flip = random.nextInt(2);
@@ -19,6 +20,12 @@ public class inMessageCommands extends ListenerAdapter {
             coin = flip==0?"heads":"tails";
 
             event.getMessage().reply(coin).queue();
+        }
+
+        if (event.getMessage().getContentRaw().equals("?roll")){
+            int roll = random.nextInt(1,7);
+
+            event.getMessage().reply(String.valueOf(roll)).queue();
         }
     }
 }
